8de09e527a89d8958925c46e5b399bdcede0a732
Mulagura <mulagura@users.noreply.github.com>
2019-02-18 16:36:21 +0530

    Update armstrong.js

--- Js-Cookbook/armstrong.js
+++ Js-Cookbook/armstrong.js
@@ -1,42 +1,43 @@
 // armstrong
+// a number that is the sum of its own digits each raised to the power of the number of digits.
 // 123 output: 1^3+2^3+3^3 = 1+8+27 = 36
 
+// Solution: 1
 const number = 123;
 
-//console.log(Math.pow(number,3)); Math.pow(base,exponent)
-
-//console.log(number.toString().length);
+//Math.pow(base,exponent);  base^exponent
+// Number('string')  --> converts provided string to number 
+// split()           --> string to array
+// toString() 
 
 let lengthOfNumber = number.toString().length;
 var result = 0;
 
 for(var i=0;i<lengthOfNumber;i++){
     var eachNumberArray = number.toString().split('');
-    //console.log(eachNumberArray);  
     eachNumberArray[i] =  Math.pow(Number(eachNumberArray[i]),lengthOfNumber);
     result+=eachNumberArray[i];
-    
 }
 
 console.log(result);
 
 // simplified version
 
-// armstrong
+// Solution: 2
 
 // 1634 = 1^4 + 6^4 + 3^4 + 4^4 = 1+1296++81+256 = 1634
 
 let armStrong = (num)=>{
 
     let count = num.toString().length;
-    //console.log(count); // 4
+  
     let eachNumber = num.toString().split('');
-    //console.log(eachNumber); // ["1", "6", "3", "4"]
+    
     let sumOf = 0;
     for(let i=0;i<count;i++){
         sumOf = sumOf + Math.pow(eachNumber[i],count);
     }
-    // console.log(sumOf); // 1634
+    
     if(sumOf === num){
         console.log(`Given ${num} is Armstrong`);
     }
@@ -49,25 +50,15 @@ let armStrong = (num)=>{
 
 armStrong(1634);
 
-let armStrong1 = (num)=>{
 
-    let toStrng = num.toString(); 
-    let legth = toStrng.length;
-    let count = 0;
+// Solution: 3 advanced
 
-    for(let i=0;i<legth;i++){
-        count+=Math.pow(toStrng.charAt(i),legth);
-    }
-    //console.log(count);
+const sal = 2222;
+var salLength = sal.toString().length;
 
-    if(count === num){
-        console.log(`Given ${num} is Armstrong`);
-    }
-    else{
-        console.log(`Not an Armstrong`);
-    }
-    
+var totalSal = 0;
 
-};
-
-armStrong1(1634);
+for(var i=0;i<salLength;i++){
+    totalSal = totalSal + Math.pow(Number(sal.toString()[i]),salLength);
+}
+console.log(totalSal);//64
