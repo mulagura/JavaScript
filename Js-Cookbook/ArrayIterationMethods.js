e696d6aaeb108146e36f8e787e65b17462fdb9b0
Mulagura <mulagura@users.noreply.github.com>
2019-02-19 11:52:03 +0530

    Update ArrayIterationMethods.js

--- Js-Cookbook/ArrayIterationMethods.js
+++ Js-Cookbook/ArrayIterationMethods.js
@@ -1,4 +1,5 @@
                                   // Array Iteration Methods
+// https://medium.com/@JeffLombardJr/understanding-foreach-map-filter-and-find-in-javascript-f91da93b9f2c                                  
 // forEach(callback);
 // map(callback) 
 // filter()
@@ -12,6 +13,7 @@
       // executes the provided callback once for each element present in the array in ascending order
       // There is no way to stop or break a forEach() loop other than by throwing an exception       
   */
+// forEach.js 
  
   const HollyWoodMovies = ['Apocalypto','Passion of christ','MI','Ghost Rider','Thor','Avnegers'];
 
@@ -26,28 +28,23 @@
         }
   });
 
-  console.log(HollyWoodMovies);
+  console.log(HollyWoodMovies); // ['Apocalypto','Passion of christ','MI','Ghost Rider','Thor','Avnegers']
  
-// for vs forEach  
-   // if the condition contains <= instead of <. As a result, for loop will behaves diifferently,
-   // in for loop using keyword 'break' can come out of loop, but not in forEach; we should throw exception
-   // scoping; forEach has its own this args
-   // When using the .forEach() you pass an individual function with it’s own scope.
-   // In a for loop you’re polluting whatever scope you place the loop in.  
 
 /* 2. map(function(element,index,wholeArray){
 
 });
     // creates a new array with the results of calling function on every element in the calling array
     // .map() when you want to transform elements in an array.
-  */
+
+  */ // map.js
 
 const oddNumbers = [1,3,5,7,9];
 const sqrOfOddNumbers = oddNumbers.map((number,index,arr)=>{
     return Math.pow(number,2);
 });
 
-console.log(sqrOfOddNumbers);
+console.log(sqrOfOddNumbers); // [ 1, 9, 25, 49, 81 ]
 
 /* 3. filter((element,index,array)=>{
 
@@ -64,7 +61,7 @@ const filteredAges = orphanAges.filter((age,index,arr)=>{
             return age;
         }
 });
-console.log(filteredAges);
+console.log(filteredAges); // [ 20, 19 ]
 
 /* 4. reduce((Accumulator,Current Value,Current Index, Source Array )=>{
 
@@ -74,10 +71,13 @@ console.log(filteredAges);
        it is the accumulated value previously returned in the last invocation of the callback, or initialValue
     // .reduce() when you want derive a single value from multiple elements in an array.
  */
+// reduce.js
+// https://www.youtube.com/watch?v=g1C40tDP0Bk
 
 const values = [1,3,5,78,8,8,545,4,4];
 const reducerFunc = values.reduce((acc,cur,index,arr)=>{
         return acc+cur;
+        
 }); 
 console.log(reducerFunc); //656
 
@@ -93,11 +93,14 @@ console.log(reducerFunc); //656
     // Otherwise undefined is returned
     // .find() When you want to select a single element from an array.
 
+    // filter returns an array , while find return an object
+    // filter scans the entire array and creates another array
+
 const objs = [{id:1,total:85},{id:2,total:93},{id:3,total:97},{id:4,total:67},{id:5,total:90},];
 const letsFind = objs.find((student,index,arr)=>{
         return student.total <= 80;
 });
-console.log(letsFind);
+console.log(letsFind); // { id: 4, total: 67 }
 
 /* 6. findIndex((element,index,array)=>{
 
@@ -114,3 +117,7 @@ console.log(letsFind);
  
  console.log(letsFindIndex); // -1 
  
+ var ages = [3, 10, 18, 20];
+ console.log(ages.indexOf(10)); // 1
+
+ console.log( [1, 2, 3].includes(2) );      // true
