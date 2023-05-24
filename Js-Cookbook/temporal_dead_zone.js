e1d93595b553fef69f957f1e0026566f639b0945
Mulagura <mulagura@users.noreply.github.com>
2023-05-24 12:00:32 -0400

    Create temporal_dead_zone.js

---
+++ Js-Cookbook/temporal_dead_zone.js
@@ -0,0 +1,17 @@
+https://www.freecodecamp.org/news/what-is-the-temporal-dead-zone/
+
+A let or const variable is said to be in a "temporal dead zone" (TDZ) 
+from the start of the block until code execution reaches the line where the variable is declared and initialized. 
+While inside the TDZ, the variable has not been initialized with a value, and any attempt to access it will result in a ReferenceError
+
+eg:
+
+ {
+ // tdz
+  // tdz
+  console.log(a); // ReferenceError
+  // but if we use var, it will be undifined, becaus of hoisting
+  // let and const have hoisting
+  let a;
+  console.log(a); // undefined
+}
