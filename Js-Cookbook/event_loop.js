ca240f2e63be0ad3ed5e68e511a8fcd060180199
Mulagura <mulagura@users.noreply.github.com>
2023-05-27 11:46:53 -0400

    event_loop.js

---
+++ Js-Cookbook/event_loop.js
@@ -0,0 +1,6 @@
+// Event Loop:
+// https://www.geeksforgeeks.org/what-is-an-event-loop-in-javascript/
+An event loop is something that pulls stuff out 
+of the queue and places it onto the function execution stack whenever the function stack becomes empty.
+
+The event loop is the secret by which JavaScript gives us an illusion of being multithreaded even though it is single-threaded.
