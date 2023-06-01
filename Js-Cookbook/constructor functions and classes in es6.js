af5b9b799fda293ba3e6e0906c2e28aac201985f
Mulagura <mulagura@users.noreply.github.com>
2023-06-01 11:16:37 -0400

    Update constructor functions and classes in es6.js

--- Js-Cookbook/constructor functions and classes in es6.js
+++ Js-Cookbook/constructor functions and classes in es6.js
@@ -1,2 +1,11 @@
 // https://www.vojtechruzicka.com/javascript-constructor-functions-and-new-operator/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor
+
+// ES6
+
+// Destructuring
+--> unpack values from arrays, or properties from objects, into distinct variables.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+// export import in module ,es6
+// https://www.digitalocean.com/community/tutorials/understanding-modules-and-import-and-export-statements-in-javascript
