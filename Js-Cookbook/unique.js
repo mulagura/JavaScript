f3140ae1fb5c08b6b5886f3357d69488ce9ce5bf
Mulagura <mulagura@users.noreply.github.com>
2023-11-14 15:55:36 -0500

    Update unique.js

--- Js-Cookbook/unique.js
+++ Js-Cookbook/unique.js
@@ -7,16 +7,16 @@ const arr = [
 ];
 
 // unique values from array
-const arr = [1, 2, 3, 1, 2, 4, 5, 3, 5, 5];
+const arrNum = [1, 2, 3, 1, 2, 4, 5, 3, 5, 5];
 
-const unique = arr.filter((ele,indx) => {
-	return arr.indexOf(ele) === indx;
+const unique = arrNum.filter((ele,indx) => {
+	return arrNum.indexOf(ele) === indx;
 }); 
 console.log(unique);
 
 // unique values with count from array
 const uniqueCount = unique.map((item) => {
-	console.log(`${item} : ${arr.filter(el => el === item).length}`);
+	console.log(`${item} : ${arrNum.filter(el => el === item).length}`);
 });
 
 // unique array of objects both es5 and es6
